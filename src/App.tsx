316accb91274fb2edcdaad406d5b61e363e5eef1
rumen <osarumen668@gmail.com>
2024-12-12 08:53:14 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+      <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-400">
+          Hello world!
+      </h1>
+  )
+}
+
+export default App
