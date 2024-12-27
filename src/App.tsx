0e2881ace56c56ab8f72192f4d2d38cee803c198
rumen <osarumen668@gmail.com>
2024-12-27 14:15:47 -0500

    Made the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {TileProvider} from "./Context/TileContext.tsx";
 import {SpeedProvider} from "./Context/speedContext.tsx";
 import {Grid} from "./Components/grid.tsx";
 import {useRef} from "react";
+import {Nav} from "./Components/Nav.tsx";
 
 function App() {
     const isVisualizationRunningRef = useRef<boolean>(false)
@@ -12,6 +13,7 @@ function App() {
           <TileProvider>
               <SpeedProvider>
           <div className="h-screen w-screen flex flex-col">
+            <Nav/>
             <Grid isVisualizationRunningRef={isVisualizationRunningRef}/>
           </div>
               </SpeedProvider>
---
+++ src/Components/Nav.tsx
@@ -0,0 +1,14 @@
+import "./Nav.css"
+
+export function Nav() {
+    return(
+        <div className="Navbar">
+            <div className="Namebar">
+            <h1 className="Title">
+                PathFinding Visualizer
+            </h1>
+            <p className="Author">by Osarumen Izedonmwen</p>
+            </div>
+        </div>
+    )
+}
